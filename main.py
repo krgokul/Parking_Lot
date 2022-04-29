997dacb2a6c0b79dea56ffcf0411587466d4416c
Gokul <gokul.kr@bigthinkcode.com>
2022-04-29 12:10:23 +0530

    Added Receipt class.

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import datetime
 
 class Spot:
-    def __init__(self,name,vehicle_type,is_free):
+    def __init__(self,name,vehicle_type,is_occupied):
         self.name = name
         self.vehicle_type = vehicle_type
         self.vehicle_id = ""
-        self.is_free= is_free
+        self.is_occupied = is_occupied
 
-    def set_filled(self):
-        self.is_free = True
+    def set_spot_status(self,spot_status):
+        self.is_occupied = spot_status
     
-    def set_empty(self):
-        self.is_free = False
     
     def __str__(self):
-        return "{} {} {} {}".format(self.name,self.vehicle_id,self.vehicle_type,self.is_free)    
+        return "{} {} {} {}".format(self.name,self.vehicle_id,self.vehicle_type,self.is_occupied)    
 
 class Floor:
     def __init__(self,name,entry,exit):
@@ -103,30 +101,17 @@ class System:
                 print (j.name,j.is_free)
             print("-----------")
     
-    def assign_spot_to_vehicle(self,floor_name,spot_name,vehicle_id):
-        floor = [i for i in self.floors if i.name == floor_name]
-        spot = [j for j in floor[0].spots if j.name == spot_name]
-        spot[0].vehicle_id = vehicle_id
-        spot[0].set_filled()
-        
-    def get_ticket(self,floor_name,spot_name,vehicle_id):
+    def choose_entrance(self,floor_name):
         en=""
         for i in self.floors:
            if i.name == floor_name:
                 entrance =  i.get_entry_list()
                 for j in range (len(entrance)):
                     print("{} - {} ".format(j,entrance[j]))
-                en= entrance[int(input())]       
-        self.assign_spot_to_vehicle(floor_name,spot_name,vehicle_id)
-        return Ticket(vehicle_id,floor_name,spot_name,en,datetime.datetime.now())
+                en= entrance[int(input())]   
+        return en
     
-    def free_spot(self,floor_name,spot_name):
-        floor = [i for i in self.floors if i.name == floor_name]
-        spot = [j for j in floor[0].spots if j.name == spot_name]
-        spot[0].vehicle_id = ""
-        spot[0].set_empty()
-    
-    def exit_vehicle(self,ticket):
+    def choose_exit(self,floor_name):
         ex=""
         for i in self.floors:
            if i.name == ticket.floor_name:
@@ -134,19 +119,40 @@ class System:
                 for j in range (len(exit_list)):
                     print("{} - {} ".format(j,exit_list[j]))
                 ex = exit_list[int(input())]
-        self.free_spot(ticket.floor_name,ticket.spot_name)
-        price = self.calculate_price(ticket.entry_date)
+        return ex
+    
+    def assign_spot_to_vehicle(self,floor_name,spot_name,vehicle_id):
+        floor = [i for i in self.floors if i.name == floor_name]
+        spot = [j for j in floor[0].spots if j.name == spot_name]
+        spot[0].vehicle_id = vehicle_id
+        spot[0].set_spot_status(True)
+        
+    def get_ticket(self,floor_name,spot_name,entrance_name,vehicle_id):
+        self.assign_spot_to_vehicle(floor_name,spot_name,vehicle_id)
+        return Ticket(vehicle_id,floor_name,spot_name,entrance_name,datetime.datetime.now())
+    
+    def free_spot(self,floor_name,spot_name):
+        floor = [i for i in self.floors if i.name == floor_name]
+        spot = [j for j in floor[0].spots if j.name == spot_name]
+        spot[0].vehicle_id = ""
+        spot[0].set_spot_status(False)
+    
+    def payment_mode(self):
+        payment_mode=["Credit","Cash"]
         print("0 - Cash\t1 - Credit")
         n = int(input())
-        mode=""
-        if (n):
-            mode = "Credit"
-        else:
-            mode = "Cash"
-        p1 = Payment(price, mode)   
-        print(p1.initiate_payment())
+        return payment_mode[n]
         
-
+    def exit_vehicle(self,ticket,exit_name,payment_type):
+        self.free_spot(ticket.floor_name,ticket.spot_name)
+        price = self.calculate_price(ticket.entry_date)
+        if payment_type == "Credit":
+            pay = Credit(price)
+        else:
+            pay = Cash(price)
+        result = pay.initiate_payment()
+        return Receipt(ticket,exit_name,datetime.datetime.now(),result[0],result[1])
+    
     def calculate_price(self,ticket_date):
         price=0
         second = int(ticket_date.second)
@@ -162,6 +168,45 @@ class System:
             price = (total-3) * 25 + 100
         return price
 
+class Payment:
+    def __init__(self,price):
+        self.price= price
+        self.payment_status = False
+
+class Credit(Payment):
+    def __init__(self,price):
+        super().__init__(price)
+        self.tax = 10
+
+    def initiate_payment(self):
+        total_amount = self.price + self.tax
+        self.payment_status = True
+        return (total_amount,self.payment_status)
+        
+class Cash(Payment):
+    def __init__(self,price):
+        super().__init__(price)
+        self.deduction = 10
+
+    def initiate_payment(self):
+        total_amount = self.price + self.deduction
+        self.payment_status = True
+        return (total_amount,self.payment_status)
+
+class Receipt:
+    def __init__(self,entry_ticket,exit_name,exit_date,total,payment_status):
+        self.entry_ticket = entry_ticket
+        self.exit_name = exit_name
+        self.exit_date = exit_date
+        self.total = total
+        if (payment_status == True):
+            self.payment_status = "Paid"
+        else:
+            self.payment_status = "Not Paid"
+    def __str__(self):
+        return "{} {} {} {} {}".format(self.entry_ticket,self.exit_name,self.exit_date,self.total,self.payment_status)
+
+    
 class Admin:
     def __init__(self,name,system):
         self.name = name
@@ -191,27 +236,29 @@ class Ticket:
     def __str__(self):
         return "{} {} {} {} {} {}".format(self.id,self.vehicle_id,self.floor_name,self.spot_name,self.entry_name,self.entry_date)
 
-class Payment:
-    def __init__(self,price,mode):
-        self.price= price
-        self.mode = mode
-        self.payment_status= False
 
-    def initiate_payment(self):
-        self.payment_status=True
-        return "Payment successfull with {}".format(self.mode)
+
+
 
 
 system = System()
 admin = Admin("A1",system)
 v1 = Vehicle("TN60 1234","Bike")
 C1 = Customer(v1,system)
+
 admin.sys_obj.add_floor("F1",[],[])
 admin.sys_obj.add_floor("F2",[],[])
 admin.sys_obj.add_spot("F1","F1S1","Bike",False)
 admin.sys_obj.add_spot("F1","F1S2","Bike",False)
 admin.sys_obj.add_entry_to_floor("F1")
 admin.sys_obj.add_exit_to_floor("F1")
-C1.ticket = C1.sys_obj.get_ticket("F1","F1S1",C1.vehicle_obj.id)
+
+entrance_name = C1.sys_obj.choose_entrance("F1") 
+C1.ticket = C1.sys_obj.get_ticket("F1","F1S1",entrance_name,C1.vehicle_obj.id)
+
 C1.sys_obj.display_floors_and_spots()
-C1.sys_obj.exit_vehicle(C1.ticket)
+
+exit_name = C1.sys_obj.choose_entrance("F1") 
+payment_type = C1.sys_obj.payment_mode() 
+result = C1.sys_obj.exit_vehicle(C1.ticket,exit_name,payment_type)
+print(result)
